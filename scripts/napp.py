4c357a786fe32b5efa537dfb9967984d7dbb75d5
Michael Rose Jr <michael@michaelrosejr.com>
2017-12-11 14:09:49 -0800

    Fixed loop issue in napp.py. It now correctly builds JSON output file

--- scripts/napp.py
+++ scripts/napp.py
@@ -20,7 +20,7 @@ hp_procurve = {
 
 def main():
     # create logger
-    logging.basicConfig(filename='napp.log',level=logging.INFO)
+    logging.basicConfig(filename='napp.log', level=logging.INFO)
     # logger = logging.getLogger('simple_example')
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
@@ -38,7 +38,6 @@ def main():
     # add ch to logger
     logger.addHandler(ch)
 
-
     # hostfile = '/Users/mrose/DropboxAruba/305-apnames.csv'
     hostfile = 'test.csv'
 
@@ -52,9 +51,8 @@ def main():
         #
         #     apnames.append(apname)
         #     controllerips.append(controllerip)
-        apnames = {rows[0]:rows[1] for rows in readcsv}
+        apnames = {rows[0]: rows[1] for rows in readcsv}
         # print(apnames)
-
 
     # print(apnames)
 
@@ -64,13 +62,7 @@ def main():
 
     get_user_ap_diff(newapnames, logger)
 
-
     # pp.pprint(newapnames)
-
-
-
-
-
 
 def hpswitch():
 
@@ -81,7 +73,7 @@ def hpswitch():
 
     name = None
     for i, line in enumerate(output):
-        r = random.randint(0,i)
+        r = random.randint(0, i)
         if not r and line.strip():
             name = line.split()[0]
     print(name)
@@ -91,8 +83,8 @@ def show_arp():
     net_connect = ConnectHandler(**aruba_os)
     output = net_connect.send_command("show arp")
 
-    #print(output)
-    output = output.split("\n",3)[3];
+    # print(output)
+    output = output.split("\n", 3)[3]
     output = output[:output.rfind('\n')]
 
     data = output.splitlines(True)
@@ -100,7 +92,7 @@ def show_arp():
         columns = line.split()
         arp = {}
         arp['ip'] = columns[1]
-        arp['mac'] =  columns[2]
+        arp['mac'] = columns[2]
         arp['int'] = columns[3]
         print(json.dumps(arp))
 
@@ -110,10 +102,10 @@ def show_dhcp_stats():
     net_connect = ConnectHandler(**aruba_os)
     output = net_connect.send_command("show ip dhcp statistics")
 
-    output = output.split("\n",7)[7];
+    output = output.split("\n", 7)[7]
     output = output[:-2]
 
-    #print(output)
+    # print(output)
     data = output.splitlines(True)
     for line in data:
 
@@ -125,7 +117,7 @@ def show_dhcp_stats():
             columns = line.split()
             dhcp['network'] = columns[0]
             dhcp['type'] = columns[1]
-            dhcp['active'] =  columns[2]
+            dhcp['active'] = columns[2]
             dhcp['total'] = columns[3]
             dhcp['used'] = columns[4]
             dhcp['free'] = columns[5]
@@ -136,6 +128,7 @@ def get_user_ap_diff(apnames, logger):
     exportdata = {}
 
     for cip, apnames in apnames.items():
+        exportdata[cip] = {}
         aruba_os = {
             'device_type': 'aruba_os',
             'ip': cip[1:],
@@ -148,8 +141,8 @@ def get_user_ap_diff(apnames, logger):
         logger.info("Starting query on: %s", cip[1:])
         net_connect = ConnectHandler(**aruba_os)
 
-        show_ap_assoc= "show ap association | include Num"
-        show_usertable="show user-table | include Entries"
+        show_ap_assoc = "show ap association | include Num"
+        show_usertable = "show user-table | include Entries"
         ap_assoc_num_clients = net_connect.send_command(show_ap_assoc)[:-1]
         user_table_count = net_connect.send_command(show_usertable)
         user_table_count = user_table_count[1:-1]
@@ -167,17 +160,16 @@ def get_user_ap_diff(apnames, logger):
         #
         usertable_count = usertable[1].split("/")
         logger.info("Number of users in table: %s", usertable_count[1])
-        exportdata['num_of_users'] = usertable_count[1]
+        exportdata[cip]['num_of_users'] = usertable_count[1]
 
         showap_count = ap_assoc_num_clients.split(":")
         logger.info("Number of client associated to APs: %s", showap_count[1])
-        exportdata['num_of_clients_associated'] = showap_count[1]
+        exportdata[cip]['num_of_clients_associated'] = showap_count[1]
         if showap_count[1] != "":
             if usertable_count[1] != "":
                 diffuser = int(showap_count[1]) - int(usertable_count[1])
         logger.info("Diff between client associated and usertable: %s", diffuser)
-        exportdata['diff_user_count'] = diffuser
-
+        exportdata[cip]['diff_user_count'] = diffuser
 
         for apname in apnames:
             show_usertable_apname = "show user-table ap-name " + apname + " unique | include Entries"
@@ -186,16 +178,16 @@ def get_user_ap_diff(apnames, logger):
             data_usertable_apname_count = data_usertable_apname.split(':')
             # print(data_usertable_apname)
 
-            logger.info("{},{},{}".format(cip[1:],apname, data_usertable_apname_count[1][1:]))
-            exportdata['controllerip'] = cip[1:]
-            exportdata[apname] = data_usertable_apname_count[1][1:]
+            logger.info("{},{},{}".format(cip[1:], apname, data_usertable_apname_count[1][1:]))
+            # exportdata['controllerip'] = cip[1:]
+            exportdata[cip][apname] = data_usertable_apname_count[1][1:]
 
         logger.info("Completed query on: %s", cip[1:])
-
 
     # pp.pprint(exportdata)
     with open('nordcontout.json', 'w') as outfile:
         json.dump(exportdata, outfile)
+
 
 if __name__ == "__main__":
     main()
